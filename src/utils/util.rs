23b1f676d4222d90a451d6494482d13a22b74c87
jinthium <futureiswap2901@gmail.com>
2024-05-26 19:58:37 -0400

    fix

--- src/utils/util.rs
+++ src/utils/util.rs
@@ -2,6 +2,6 @@ pub struct Util {}
 
 impl Util {
     pub fn get_env_var(var_name: &str) -> String {
-        return std::env::var(var_name).expect(format!("Couldnt find environment variable: {var_name}").as_str())
+        return std::env::var(var_name).expect(&format!("Couldnt find environment variable: {var_name}"))
     }
 }
